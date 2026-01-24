d90520955d6d4fe800f7c152c525e821a444536b
Praveen Kumar G <praveen06061995@gmail.com>
2026-01-24 13:11:29 +0530

    Feat: Initial commit for frontend.

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className='bg-red-700'>
+        helloo
+      </div>
+    </>
+  )
+}
+
+export default App
