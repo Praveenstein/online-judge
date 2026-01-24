045ef0856a3dcceb10b33b0f3b0888c605b2b94f
Praveen Kumar G <praveen06061995@gmail.com>
2026-01-24 14:14:49 +0530

    Feat: Add login page.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,10 @@
-import { useState } from 'react'
-import './App.css'
+import Auth from './components/Auth'
 
 function App() {
   return (
-    <>
-      <div className='bg-red-700'>
-        helloo
-      </div>
-    </>
+    <div className="App">
+      <Auth />
+    </div>
   )
 }
 
